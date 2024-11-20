ec41ef94b72f841432aca4f7172b095f3b76ce87
baekminjin <bhs3392@naver.com>
2024-11-20 17:43:45 +0900

    목록

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	public static void main(String[] args) {
-		App app = new App();
-		app.run();
+		App app = new App(); //객체 생성
+		app.run(); //run함수 호출
 
 	}
 }
 
-	class App{
+	class App{ //템플릿
         //목록을 출력하기 위해 등록된 리스트를 저장할 장소
-		ArrayList<String> megs = new ArrayList<>();
-		ArrayList<String> names = new ArrayList<>();
+		ArrayList<String> contents = new ArrayList<>();
+		ArrayList<String> authors = new ArrayList<>();
 
 		public void run() {
 			System.out.println("== 명언 앱 ==");
@@ -24,25 +24,31 @@ public class Main {
 			//스캐너(System.in(키보드)를 스캔하는 객체 생성
 			Scanner scanner = new Scanner(System.in);
 
-			int cnt =0; //while문에 넣으면 반복마다 초기화가 된다.
+			int cnt =0; //반복문에 넣으면 반복마다 초기화가 된다.
 
 			while (true) {
 
 				System.out.print("명언) ");
-				String cmd = scanner.nextLine();
+				String cmd = scanner.nextLine(); //키보드에서 입력한 것을 cmd로 저장
 
-				if(cmd.equals("등록")){
+				if(cmd.equals("등록")){ //저장된 입력값 cmd와 비교
 					System.out.print("명언: ");
-					String meg = scanner.nextLine();
+					String content = scanner.nextLine();//입력 값 meg 저장
 
 					System.out.print("작가: ");
-					String name = scanner.nextLine();
+					String author = scanner.nextLine(); //입력 값 name 저장
 
-					cnt++; //번호 증가
-					megs.add(meg); //명언 리스트 추가
-					names.add(name);
+					int id = ++cnt; // 위의 입력이 끝날 시 번호 증가
 
-					System.out.println(cnt+"번 명언이 등록되었습니다. ");
+					WiseSaying wiseSaying = new WiseSaying();
+					wiseSaying.id = id; //힙에다 값을 전달
+					wiseSaying.content = content;
+					wiseSaying.author = author;
+
+					contents.add(content); //명언 리스트 추가
+					authors.add(author);
+
+					System.out.println(id+"번 명언이 등록되었습니다. ");
 				}
 
 
@@ -50,8 +56,8 @@ public class Main {
 				else if(cmd.equals("목록")){
 					System.out.println("번호 / 작가 / 명언");
 					System.out.println("---------------------");
-					for (int i = megs.size() - 1; i >= 0; i--) {
-						System.out.println((megs.size() - i) + " / " + names.get(i) + " / " + megs.get(i));
+					for (int i = 0; i< contents.size(); i++) {
+						System.out.println((contents.size() - i) + " / " + authors.get(contents.size()-1-i)+ " / " + contents.get(contents.size()-1-i));
 				}
 				}
 
@@ -67,4 +73,11 @@ public class Main {
 
 		}
 	}
+
+//한꺼번에 옮기기 위해
+		class WiseSaying {
+			int id;
+			String content;
+			String author;
+		}
 
